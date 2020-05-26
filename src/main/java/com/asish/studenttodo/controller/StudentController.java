d7418432882f2b9fc1ffc842aaa4a0b2d1c74abf
Asish Samantaray <samantaray.asish04@gmail.com>
2020-05-27 02:14:48 +0530

    Controller method for Get All Students from the DB has been added.

--- src/main/java/com/asish/studenttodo/controller/StudentController.java
+++ src/main/java/com/asish/studenttodo/controller/StudentController.java
@@ -5,10 +5,9 @@ import com.asish.studenttodo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -23,5 +22,11 @@ public class StudentController {
     @PostMapping("/add-student")
     public ResponseEntity<Student> addStudent(@RequestBody Student student) {
         return new ResponseEntity<>(studentService.addStudent(student), HttpStatus.OK);
+    }
+
+    // Controller method for Get All Students from the DB.
+    @GetMapping("/get-student")
+    public ResponseEntity<List<Student>> getAllStudent() {
+        return new ResponseEntity<>(studentService.getAllStudent(), HttpStatus.OK);
     }
 }
