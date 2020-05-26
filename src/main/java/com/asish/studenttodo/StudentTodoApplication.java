d27a86da6bb40c4e3d72fdc3a1fb9558f086dba7
Asish Samantaray <samantaray.asish04@gmail.com>
2020-05-27 00:33:22 +0530

    Initial Commit

---
+++ src/main/java/com/asish/studenttodo/StudentTodoApplication.java
@@ -0,0 +1,13 @@
+package com.asish.studenttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asish/studenttodo/StudentTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asish.studenttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
