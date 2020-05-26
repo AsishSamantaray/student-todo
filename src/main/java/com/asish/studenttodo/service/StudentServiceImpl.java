2f5444ddfc0f1f7011aa910bcc7c90d1e5939bfd
Asish Samantaray <samantaray.asish04@gmail.com>
2020-05-27 01:53:39 +0530

    Add/Save new Student into DataBase.

--- src/main/java/com/asish/studenttodo/service/StudentServiceImpl.java
+++ src/main/java/com/asish/studenttodo/service/StudentServiceImpl.java
@@ -1,13 +1,21 @@
 package com.asish.studenttodo.service;
 
 import com.asish.studenttodo.entity.Student;
+import com.asish.studenttodo.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public Student addStudent(Student student) {
+        studentRepository.save(student);
         return student;
     }
 }
